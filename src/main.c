301b9402aa0cd8f97358a2fd30ff055591e038e5
mateus-sartorio <mateussartorio39@gmail.com>
2024-06-19 22:57:07 -0300

    7: Processing inside the SysTick Interrupt

--- src/main.c
+++ src/main.c
@@ -15,18 +15,46 @@
 #include <stdint.h>
 
 #include "../include/led.h"
+#include "clock_efm32gg.h"
 #include "em_device.h"
+
+#define true 1
+#define false 0
 
 /**
  * @brief  SysTick is called every 1 ms
  */
-#define DIVIDER 1000
+#define SYSTICKDIVIDER 1000
 
 #define DELAYVAL 1000
 
+#define SOFTDIVIDER 1000
+
 volatile uint32_t tick_counter = 0;
 
-void SysTick_Handler(void) { tick_counter++; }
+void SysTick_Handler(void) {
+    static int counter = 0;
+    static int8_t state = 0;
+
+    if(counter != 0) {
+        counter--;
+    } else {
+        switch(state) {
+            case 0:
+                LED_Toggle(LED1);
+                state = 1;
+                break;
+            case 1:
+                LED_Toggle(LED2);
+                state = 0;
+                break;
+            default:
+                break;
+        }
+
+        counter = SOFTDIVIDER - 1;
+    }
+}
 
 void Delay(uint32_t delay_ammount) {
     volatile uint32_t initial_value = tick_counter;
@@ -43,14 +71,13 @@ void DelayPulses(uint32_t delay) {
 }
 
 int main(void) {
+    (void)SystemCoreClockSet(CLOCK_HFXO, 1, 1);
+
     LED_Init(LED1 | LED2);
 
-    SysTick_Config(SystemCoreClock / DIVIDER);
+    SysTick_Config(SystemCoreClock / SYSTICKDIVIDER);
 
     __enable_irq();
 
-    while(1) {
-        Delay(DELAYVAL);
-        LED_Toggle(LED1 | LED2);
-    }
+    while(true);
 }
