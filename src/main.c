e27eeff9dbaac0a8e5b3f00e7545ce979d09566a
mateus-sartorio <mateussartorio39@gmail.com>
2024-08-04 14:07:02 -0300

    Formatter configured

--- src/main.c
+++ src/main.c
@@ -16,101 +16,18 @@
 #include <stdint.h>
 
 #include "../include/button.h"
+#include "../include/i2cmaster.h"
 #include "../include/lcd.h"
 #include "../include/led.h"
-#include "em_device.h"
-#include "../include/i2cmaster.h"
 #include "../include/utils.h"
+#include "em_device.h"
 
 #define DELAYVAL 100
 #define MPU_ADDR 0x68
-#define VALIDATOR 0x68
+#define MPU_WHO_AM_I 0x75
 
-void Delay(uint32_t delay)
-{
-    volatile uint32_t counter;
-    int i;
+int main(void) {
+    int a = 2;
 
-    for (i = 0; i < delay; i++)
-    {
-        counter = 10000;
-        while (counter)
-        {
-            counter--;
-        }
-    }
-}
-
-void WriteMultiple(uint8_t ch)
-{
-    int pos;
-
-    for (pos = 1; pos <= 11; pos++)
-    {
-        LCD_WriteChar(ch, pos);
-    }
-}
-
-int main(void)
-{
-    // /* Configure Pins in GPIOE */
-    // LED_Init(LED1 | LED2);
-
-    // /* Configure LCD */
-    // LCD_Init();
-
-    // LCD_SetAll();
-    // Delay(DELAYVAL);
-
-    // LCD_ClearAll();
-    // Delay(DELAYVAL);
-
-    int rc = I2CMaster_Init(I2C1, 100, 0);
-
-    uint8_t data[2];
-    data[0] = 0x6B; // Registrador PWR_MGMT_1
-    data[1] = 0x00; // Valor para acordar o MPU6050
-    rc = I2CMaster_SendStart(I2C1, VALIDATOR, data, 2);
-
-    // char string[10];
-    // itoa(rc, string, 10);
-    // char *s = string;
-
-    /* Blink loop */
-    while (1)
-    {
-        // Delay(DELAYVAL);
-        // LED_Toggle(LED1); // Toggle LED1
-        // LCD_WriteAlphanumericDisplay(s++);
-
-        // if (*s == '\0')
-        // {
-        //     s = string;
-        // }
-
-        // Delay(DELAYVAL);
-        // LED_Toggle(LED2); // Toggle LED2
-
-        // Delay(DELAYVAL);
-        // LED_Write(0, LED1 | LED2); // Turn On All LEDs
-
-        // rc = I2CMaster_Send(I2C1, VALIDADDR, data, 1);
-        // if(rc < 0) {
-        //     fprintf(stderr,"Detecting valid I2C device with address %d failed
-        //     (%d)\n",VALIDADDR,rc);
-        // } else {
-        //     fprintf(stderr,"Detected I2C device with address %d
-        //     successful\n",VALIDADDR);
-        // }
-
-        // rc = I2CMaster_Send(I2C1, NOTVALIDADDR, data, 1);
-        // if(rc < 0) {
-        //     fprintf(stderr,"Detected an I2c device with address %d
-        //     \n",NOTVALIDADDR);
-        // } else {
-        //     fprintf(stderr,"Detected an I2c device with address"
-        //                    " %d but the address should be
-        //                    invalid\n",NOTVALIDADDR);
-        // }
-    }
+    return 0;
 }
